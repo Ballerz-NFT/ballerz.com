b8434eded067aab04001c62e38f8ec5714e59f4a
Ben Yarbrough <ben.yarbrough@soundcloud.com>
2024-06-03 02:13:02 -0400

    add baller with speech bubble

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ballerz.cloud",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
