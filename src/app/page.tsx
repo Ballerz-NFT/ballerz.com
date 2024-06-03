d715be13d3bcbbc7eaf2fedee56e89bbb57a1c4b
Ben Yarbrough <ben.yarbrough@soundcloud.com>
2024-06-03 02:33:43 -0400

    logo & links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <header className="w-full text-center border-b border-slate-700">
-        <h1 className="text-4xl uppercase">Ballerz</h1>
+        {/* <h1 className="text-4xl uppercase">Ballerz</h1> */}
+        <Image
+          src="/images/logo.png"
+          width={320}
+          height={60}
+          alt="Ballerz"
+          className="mx-auto mb-2"
+        />
         <p>Now community owned!</p>
 
         <p className="speech-bubble w-64 text-xl text-black p-6 mx-auto mt-12">
@@ -19,6 +26,16 @@ export default function Home() {
           className="mx-auto"
         />
       </header>
+
+      <nav className="py-12">
+        <a href="https://x.com/BALLERZ_NFT" target="_blank">
+          Twitter
+        </a>
+        <span className="mx-3">|</span>
+        <a href="https://discord.gg/qbuMQgTf8K" target="_blank">
+          Discord
+        </a>
+      </nav>
     </main>
   );
 }
