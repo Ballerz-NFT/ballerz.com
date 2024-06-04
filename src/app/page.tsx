aa544850c03c082d7e55eb9be39f00f218761cb6
Ben Yarbrough <ben.yarbrough@soundcloud.com>
2024-06-04 02:09:46 -0400

    reduce x padding on main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center py-24 px-4">
       <header className="w-full text-center">
         {/* <h1 className="text-4xl uppercase">Ballerz</h1> */}
         <Image
